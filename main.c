7553096fdd6464ac0e8fd95cabd03faac369a2cc
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-02-11 21:58:35 +0700

    print hello world x times
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
+#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+
+void
+print_xtime(const char* s, int* x);
 
 int
 main()
 {
-  printf("hello world\n");
+  int x;
+  scanf("%d", &x);
+  print_xtime("Hello Worlds", &x);
   return 0;
+}
+
+void
+print_xtime(const char* s, int* x)
+{
+  const char* p = s;
+  for (size_t i = 0; i < *x; i++) {
+    while (*s != '\0') {
+      printf("%c",*s);
+      s++;
+    }
+    printf("\n");
+    s = p;
+  }
 }
